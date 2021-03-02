ec04665cb0a66c41fed1d345c0b7456103a18e8d
Crqbu <56938734+Crqbu@users.noreply.github.com>
2021-03-02 17:34:14 +0100

    Update Card.java

--- Prog2_tCHu/src/ch/epfl/tchu/game/Card.java
+++ Prog2_tCHu/src/ch/epfl/tchu/game/Card.java
@@ -1,24 +1,29 @@
 package ch.epfl.tchu.game;
 import java.util.List;
 
-/**
- * Card Class
- * The different colors of the cards have
- * @author Eduardo Neville
- */
 public enum Card {
-        BLACK,
-        VIOLET,
-        BLUE,
-        GREEN,
-        YELLOW,
-        ORANGE,
-        RED,
-        WHITE,
-        LOCOMOTIVE;
+
+    BLACK(Color.BLACK), VIOLET(Color.VIOLET), BLUE(Color.BLUE), GREEN(Color.GREEN), YELLOW(Color.YELLOW), ORANGE(Color.ORANGE), RED(Color.RED), WHITE(Color.WHITE), LOCOMOTIVE(null);
+
+    private final Color cardColor;
+
+    Card(Color cardColor) {
+        this.cardColor = cardColor;
+    }
 
     public static final List<Card> ALL = List.of(values());
 
     public static final int COUNT = ALL.size();
 
-}
+    public static final List<Card> CARS = List.of(BLACK, WHITE);
+
+    public static Card of(Color color){
+
+        return Card.valueOf(color.toString());
+    }
+
+    public Color color(){
+        return this.cardColor;
+    }
+
+    }
