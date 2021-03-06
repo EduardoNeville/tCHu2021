3933ed6f4a9b5a9399af35cda1116fac18c5f65a
Ixss82 <57065449+Ixss82@users.noreply.github.com>
2021-03-06 18:22:03 +0100

    Update Card.java

--- Prog2_tCHu/src/ch/epfl/tchu/game/Card.java
+++ Prog2_tCHu/src/ch/epfl/tchu/game/Card.java
@@ -1,29 +1,51 @@
 package ch.epfl.tchu.game;
 import java.util.List;
 
+/**
+ * Card Enumerated Class
+ *
+ * @author Eduardo Neville
+ * @author Hamza Karime
+ */
 public enum Card {
 
-    BLACK(Color.BLACK), VIOLET(Color.VIOLET), BLUE(Color.BLUE), GREEN(Color.GREEN), YELLOW(Color.YELLOW), ORANGE(Color.ORANGE), RED(Color.RED), WHITE(Color.WHITE), LOCOMOTIVE(null);
+        BLACK(Color.BLACK),
+        VIOLET(Color.VIOLET),
+        BLUE(Color.BLUE),
+        GREEN(Color.GREEN),
+        YELLOW(Color.YELLOW),
+        ORANGE(Color.ORANGE),
+        RED(Color.RED),
+        WHITE(Color.WHITE),
+        LOCOMOTIVE(null);
 
-    private final Color cardColor;
+        private final Color cardColor;
 
-    Card(Color cardColor) {
-        this.cardColor = cardColor;
-    }
+        Card(Color cardColor) {
+                this.cardColor = cardColor;
+        }
 
-    public static final List<Card> ALL = List.of(values());
+        public static final List<Card> ALL = List.of(values());
 
-    public static final int COUNT = ALL.size();
+        public static final int COUNT = ALL.size();
 
-    public static final List<Card> CARS = List.of(BLACK, VIOLET, BLUE, GREEN, YELLOW, ORANGE, RED, WHITE);
+        public static final List<Card> CARS = List.of(
+                BLACK,
+                VIOLET,
+                BLUE,
+                GREEN,
+                YELLOW,
+                ORANGE,
+                RED,
+                WHITE);
 
-    public static Card of(Color color){
+        public static Card of(Color color){
 
-        return Card.valueOf(color.toString());
-    }
+                return Card.valueOf(color.toString());
+        }
 
-    public Color color(){
-        return this.cardColor;
-    }
+        public Color color(){
+                return this.cardColor;
+        }
 
-    }
+}
