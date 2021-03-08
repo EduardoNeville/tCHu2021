24fc8222c60d749ae556bd963a38988cd9ac43da
Ixss82 <57065449+Ixss82@users.noreply.github.com>
2021-03-08 18:01:25 +0100

    To be completed
    We have a lot of methods and edit the ones there

---
+++ Prog2_tCHu/src/ch/epfl/tchu/game/Deck.java
@@ -0,0 +1,39 @@
+package ch.epfl.tchu.game;
+
+import ch.epfl.tchu.SortedBag;
+
+import java.util.Random;
+
+/**
+ * Deck Class
+ *
+ *
+ */
+public final class Deck <C extends Comparable<C>> {
+    private final SortedBag<C> cards;
+    public static Deck<C extends Comparable<C>> Deck<C> of(SortedBag<C> cards, Random rng){
+
+    }
+
+    /**
+     *
+     * @return true if stack is empty
+     */
+    public boolean isEmpty(){
+
+    }
+
+    /**
+     * Method topCard
+     * @return topCard of the deck
+     */
+    public C topCard(){
+
+    }
+
+    public Deck<C> withoutTopCard(){
+
+        return this.
+    }
+
+}
---
+++ Prog2_tCHu/src/ch/epfl/tchu/game/PublicCardState.java
@@ -0,0 +1,43 @@
+package ch.epfl.tchu.game;
+
+import java.util.List;
+
+/**
+ *
+ */
+public class PublicCardState {
+
+    public PublicCardState(List<Card> faceUpCards, int deckSize, int discardsSize){
+
+    }
+
+    /**
+     *
+     * @return
+    */
+    public int totalSize(){
+
+    }
+
+    /**
+     *
+     * @return 
+     */
+    public List<Card> faceUpCards(){
+
+    }
+
+    public int deckSize(){
+
+    }
+
+    public boolean isDeckEmpty(){
+
+    }
+
+    public int discardsSize(){
+
+    }
+*/
+    
+}
