c54f96b978674f3a8646847f9ddf9353e589f4f9
Crqbu <56938734+Crqbu@users.noreply.github.com>
2021-03-11 17:40:54 +0100

    Update PublicCardState.java

--- Prog2_tCHu/src/ch/epfl/tchu/game/PublicCardState.java
+++ Prog2_tCHu/src/ch/epfl/tchu/game/PublicCardState.java
@@ -1,43 +1,68 @@
 package ch.epfl.tchu.game;
 
+import ch.epfl.tchu.Preconditions;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import static ch.epfl.tchu.game.Constants.FACE_UP_CARDS_COUNT;
 
 /**
- *
+ *This class represents (part of) the state of the wagon
+ * / locomotive cards that are not in the players' hand
  */
-public class PublicCardState {
+public final class PublicCardState {
+
+    public final int deckSize;
+    public final int discardsSize;
+    public final List<Card> faceUpCards;
+
 
     public PublicCardState(List<Card> faceUpCards, int deckSize, int discardsSize){
+        Preconditions.checkArgument(faceUpCards.size()== FACE_UP_CARDS_COUNT);
+        Preconditions.checkArgument(deckSize>=0);
+        Preconditions.checkArgument(discardsSize>=0);
 
-    }
+        this.deckSize = deckSize;
+        this.discardsSize = discardsSize;
+        this.faceUpCards = List.copyOf(faceUpCards);    }
 
     /**
      *
-     * @return
-    */
+     * @return the total number of cards that are not in the players' hand, namely the 5 with the face up, those from the draw pile and those from the discard pile
+     */
     public int totalSize(){
-
+        return (FACE_UP_CARDS_COUNT + deckSize + discardsSize);
     }
 
     /**
-     *
-     * @return 
+     * @return the 5 cards face up, in the form of a list containing exactly 5 elements
      */
     public List<Card> faceUpCards(){
-
+        return faceUpCards;
     }
 
-    public int deckSize(){
+    /**
+     * @return the card face up at the given index
+     */
+    public Card faceUpCard(int slot){
+        Objects.checkIndex(slot, FACE_UP_CARDS_COUNT);
+        return faceUpCards.get(slot);
+    }
 
+    /**
+     * @return the deckSize
+     */
+    public int deckSize(){
+        return deckSize;
     }
 
     public boolean isDeckEmpty(){
-
+        return deckSize==0;
     }
 
     public int discardsSize(){
-
+        return discardsSize;
     }
-*/
-    
+
 }
