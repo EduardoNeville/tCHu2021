09da4215a601477f94289c724301f4c6724904b1
Ixss82 <57065449+Ixss82@users.noreply.github.com>
2021-03-12 13:52:03 +0100

    Update CardState.java

--- Prog2_tCHu/src/ch/epfl/tchu/game/CardState.java
+++ Prog2_tCHu/src/ch/epfl/tchu/game/CardState.java
@@ -11,16 +11,13 @@ import java.util.*;
  * Constructor:
  * @author Eduardo Neville
  */
-public class CardState extends PublicCardState{
+public class CardState extends PublicCardState {
 
     private final Deck<Card> deckofCards;
     private final SortedBag<Card> discardedCards;
 
     private CardState(Deck<Card> cardDeck, List<Card> faceUpCards, SortedBag<Card> discarded) {
-        super(faceUpCards,cardDeck.size(),discarded.size());
-
-        Preconditions.checkArgument(!cardDeck.isEmpty());
-        Preconditions.checkArgument(!faceUpCards.isEmpty());
+        super(faceUpCards, cardDeck.size(), discarded.size());
 
         this.deckofCards = cardDeck;
         this.discardedCards = SortedBag.of(discarded);
@@ -28,75 +25,68 @@ public class CardState extends PublicCardState{
 
     /**
      * Static Initialization Bloc used to find first 5 cards
+     *
      * @param deck Deck we want to find the 5 cards
      * @return first 5 cards
      */
-    public static CardState of(Deck<Card> deck){
-        Preconditions.checkArgument(deck.size()>4);
-        //CardState cardState = new CardState();
-        List<Card> faceUp = null;
-        for (int i =0 ;i<=Constants.INITIAL_CARDS_COUNT;++i){ //change 5 to constant value
+    public static CardState of(Deck<Card> deck) {
+        Preconditions.checkArgument(deck.size() > 4);
+        List<Card> faceUp = new ArrayList<>();
+        for (int i = 0; i < Constants.FACE_UP_CARDS_COUNT; ++i) { //change 5 to constant value
             faceUp.add(deck.topCard());
             deck = deck.withoutTopCard();
         }
-        return new CardState(deck,faceUp,SortedBag.of()); //add sortedbag with empty of for discarded
+        return new CardState(deck, faceUp, SortedBag.of());
     }
     //Listof(first 5 cards) use method faceUpCard
-    //rest are okay
 
     /**
-     *
      * @param slot
      * @return
      */
-    public CardState withDrawnFaceUpCard(int slot){
+    public CardState withDrawnFaceUpCard(int slot) {
+        Objects.checkIndex(slot, 5);
         Preconditions.checkArgument(!deckofCards.isEmpty());
-        Objects.checkIndex(slot,5); //To be corrected
+        List<Card> faceUpCards = new ArrayList<>(faceUpCards());
         faceUpCards.remove(slot);
-        faceUpCards.add(slot,deckofCards.topCard());
-        return new CardState(deckofCards.withoutTopCard(),faceUpCards,discardedCards);
+        faceUpCards.add(slot, deckofCards.topCard());
+        return new CardState(deckofCards.withoutTopCard(), faceUpCards, discardedCards);
     }
 
     /**
      * Method returns the top card of the deck
+     *
      * @return Top card of the deck
      */
-    public Card topDeckCard(){
+    public Card topDeckCard() {
         Preconditions.checkArgument(!deckofCards.isEmpty());
         return deckofCards.topCard();
     }
 
     /**
-     *
      * @return
      */
-    public CardState withoutTopDeckCard(){
+    public CardState withoutTopDeckCard() {
         Preconditions.checkArgument(!deckofCards.isEmpty());
-        return new CardState(this.deckofCards.withoutTopCard(),faceUpCards,discardedCards);
+        return new CardState(this.deckofCards.withoutTopCard(), faceUpCards(), discardedCards);
     }
 
     /**
-     *
      * @param rng
      * @return
      */
-    public CardState withDeckRecreatedFromDiscards(Random rng){
+    public CardState withDeckRecreatedFromDiscards(Random rng) {
         Preconditions.checkArgument(deckofCards.isEmpty());
         var shuffledCards = Deck.of(discardedCards, rng);
 
-        return new CardState(shuffledCards,faceUpCards,discardedCards);
+        return new CardState(shuffledCards, faceUpCards(), discardedCards);
     }
 
     /**
-     *
      * @param additionalDiscards
      * @return
-     *//*
-    public CardState withMoreDiscardedCards(SortedBag<Card> additionalDiscards){
-        var discardedCards = SortedBag.of(discardedCards);
-
-        return new CardState(deckofCards,faceUpCards,discardedCards);
+     */
+    public CardState withMoreDiscardedCards(SortedBag<Card> additionalDiscards) {
+        return new CardState(deckofCards, faceUpCards(), discardedCards.union(additionalDiscards));
     }
-    */
-
 }
