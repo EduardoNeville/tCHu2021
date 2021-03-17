4ffe561be5f41a0573dc8f4e91f113a2b675eb8d
Ixss82 <57065449+Ixss82@users.noreply.github.com>
2021-03-17 13:15:58 +0100

    Updated Class finished

--- Prog2_tCHu/src/ch/epfl/tchu/game/PublicPlayerState.java
+++ Prog2_tCHu/src/ch/epfl/tchu/game/PublicPlayerState.java
@@ -4,38 +4,69 @@ import ch.epfl.tchu.Preconditions;
 
 import java.util.List;
 
+/**
+ * Class PublicPlayerState
+ * Methods: ticketCount, CardCount, routes, carCount, claimPoints.
+ * 
+ * @author Eduardo Neville (314667)
+ */
 public class PublicPlayerState {
     private final int ticketCount;
     private final int cardCount;
     private final List<Route> routes;
+    private final int wagonCount;
 
-    public PublicPlayerState(int ticketCount, int cardCount, List<Route> routes){
+    /**
+     * Constructor for PublicPlayerState
+     * @param ticketCount # of tickets a player has
+     * @param cardCount # of cards a player has
+     * @param routes # of routes a player has
+     * @param wagonCount # of wagons a player has
+     */
+    public PublicPlayerState(int ticketCount, int cardCount, List<Route> routes, int wagonCount){
         Preconditions.checkArgument(!(ticketCount<0));
         this.ticketCount = ticketCount;
         this.cardCount = cardCount;
         this.routes = List.copyOf(routes);
+        this.wagonCount = wagonCount;
     }
 
+    /**
+     * Ticket getter
+     * @return # of tickets
+     */
     public int ticketCount() {
         return ticketCount;
     }
 
+    /**
+     * Card getter
+     * @return # of cards
+     */
     public int CardCount() {
         return cardCount;
     }
 
+    /**
+     * Routes getter
+     * @return List of routes
+     */
     public List<Route> routes() {
         return routes;
     }
 
-    //TODO waggon count
-    /*
+    /**
+     * Wagon getter
+     * @return # of wagons
+     */
     public int carCount(){
-        return
+        return wagonCount;
     }
-    //public int getWagonCount = carCount();
-    */
-    
+
+    /**
+     * Points earned
+     * @return # of points earned
+     */
     public int claimPoints(){
         return Constants.ROUTE_CLAIM_POINTS.get(routes.size());
     }
