6cd1dee5d24124f43802199cd2d8eed70c0fa543
Ixss82 <57065449+Ixss82@users.noreply.github.com>
2021-03-24 15:10:55 +0100

    author change

--- Prog2_tCHu/src/ch/epfl/tchu/game/StationConnectivity.java
+++ Prog2_tCHu/src/ch/epfl/tchu/game/StationConnectivity.java
@@ -3,7 +3,8 @@ package ch.epfl.tchu.game;
 /**
  * Station Connectivity  Interface
  *
- * @author Eduardo Neville Hamza Karime
+ * @author Eduardo Neville (314667)
+ * @author Martin Sanchez Lopez (313238)
  */
 public interface StationConnectivity {
     public abstract boolean connected(Station s1, Station s2);
