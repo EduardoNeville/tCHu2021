506afa2b11cf97c16dd843cf0f4083fa563cc406
Ixss82 <57065449+Ixss82@users.noreply.github.com>
2021-03-24 15:03:19 +0100

    Documented PublicGameState

--- Prog2_tCHu/src/ch/epfl/tchu/game/PublicGameState.java
+++ Prog2_tCHu/src/ch/epfl/tchu/game/PublicGameState.java
@@ -1,11 +1,21 @@
 package ch.epfl.tchu.game;
 
 import ch.epfl.tchu.Preconditions;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Copy;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
+/**
+ * PublicGameState
+ *
+ * Methods: ticketCount,canDrawTickets, cardState, canDrawCards, currentPlayerId, playerState,
+ * currentPlayerState,claimedRoutes,lastPlayer
+ *
+ * @author Eduardo Neville (314667)
+ */
 public class PublicGameState {
     private final int ticketCounT;
     private final PublicCardState cardStatE;
@@ -13,35 +23,65 @@ public class PublicGameState {
     private final  Map<PlayerId, PublicPlayerState> playerStatE;
     private final PlayerId lastPlayeR;
 
+    /**
+     * Constructor for PublicGameState
+     * @param ticketsCount # of tickets 
+     * @param cardState cards on the deck
+     * @param currentPlayerId current player
+     * @param playerState Map of playerId and PlayerState
+     * @param lastPlayer last Player
+     */
+    public PublicGameState(int ticketsCount, PublicCardState cardState,
+                            PlayerId currentPlayerId,
+                           Map<PlayerId,PublicPlayerState> playerState,
+                           PlayerId lastPlayer){
 
-    public PublicGameState(int ticketsCount, PublicCardState cardState, PlayerId currentPlayerId, Map<PlayerId, PublicPlayerState> playerState, PlayerId lastPlayer){
         Preconditions.checkArgument(ticketsCount<0);
         Preconditions.checkArgument(playerState.containsKey(2) && playerState.containsValue(2));
-        //throw nullPointerException if rest of arguments null
+
         this.ticketCounT = ticketsCount;
-        this.cardStatE = cardState;
-        this.currentPlayerID = currentPlayerId;
-        this.playerStatE = playerState;
+        this.cardStatE = Objects.requireNonNull(cardState);
+        this.currentPlayerID = Objects.requireNonNull(currentPlayerId);
+        this.playerStatE = Map.copyOf(playerState);
         this.lastPlayeR = lastPlayer; //can be null
     }
 
+    /**
+     * Ticket size
+     * @return ticket size
+     */
     public int ticketCount(){
         return ticketCounT;
     }
 
+    /**
+     * checks if you can draw a ticket
+     * @return checks if you can draw a ticket
+     */
     public boolean canDrawTickets(){
         return !(ticketCounT ==0);
     }
 
+    /**
+     *
+     * @return
+     */
     public PublicCardState cardState(){
         return new PublicCardState(cardStatE.faceUpCards(), cardStatE.deckSize(), cardStatE.discardsSize());
     }
 
+    /**
+     * 
+     * @return
+     */
     public boolean canDrawCards(){
-        //Correct use of constants?
         return (cardStatE.deckSize()+cardStatE.discardsSize()>Constants.INITIAL_TICKETS_COUNT);
     }
 
+    /**
+     * Getter for the current PlayerId
+     * @return current PlayerId
+     */
     public PlayerId currentPlayerId(){
         return currentPlayerID;
     }
@@ -50,10 +90,18 @@ public class PublicGameState {
         return new PublicPlayerState(ticketCounT, cardStatE.totalSize(),playerState(playerId).routes());
     }
 
+    /**
+     * Getter for the currentplayerState
+     * @return the playerState of the currentplayer
+     */
     public PublicPlayerState currentPlayerState(){
         return playerState(currentPlayerID);
     }
 
+    /**
+     * Gives the claimedRoutes of a player
+     * @return returns the routes of a player
+     */
     public List<Route> claimedRoutes(){
         List<PublicPlayerState> allPublicPlayerState = new ArrayList<PublicPlayerState>(playerStatE.values());
         List<Route> allRoutes = new ArrayList<>();
@@ -64,6 +112,10 @@ public class PublicGameState {
         return allRoutes;
     }
 
+    /**
+     * Getter for the lastPlayer
+     * @return last player
+     */
     public PlayerId lastPlayer(){
         return lastPlayeR;
     }
