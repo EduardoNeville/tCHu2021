41806b76573f75724ca281ba440d312ef499df6c
msl <sanchez.lopez.mn@gmail.com>
2021-03-28 16:28:10 +0200

    main loop in play(..) almost done: TODO check end conditions and win messages and clean up the whole thing

--- Prog2_tCHu/src/ch/epfl/tchu/game/Game.java
+++ Prog2_tCHu/src/ch/epfl/tchu/game/Game.java
@@ -16,16 +16,15 @@ import static ch.epfl.tchu.game.Constants.*;
 public final class Game {
 
 
-    public static void play(Map<PlayerId,Player> players, Map<PlayerId, String> playerNames, SortedBag<Ticket> tickets, Random rng) {
-        Preconditions.checkArgument(players.size() == 2 && playerNames.size() ==2);
+    public static void play(Map<PlayerId, Player> players, Map<PlayerId, String> playerNames, SortedBag<Ticket> tickets, Random rng) {
+        Preconditions.checkArgument(players.size() == 2 && playerNames.size() == 2);
 
         //Map with the players' Info classes
         Map<Player, Info> pInfo = new HashMap<>();
 
-        PlayerHandler pHandler = new PlayerHandler(players);
 
         //give the players the names of all the players and init the Info classes with the names
-        for (PlayerId id: players.keySet()){
+        for (PlayerId id : players.keySet()) {
             players.get(id).initPlayers(id, playerNames);
             pInfo.put(players.get(id), new Info(playerNames.get(id)));
         }
@@ -34,92 +33,156 @@ public final class Game {
         //init gameState
         GameState gameState = GameState.initial(tickets, rng);
 
-        //tell players who plays first
+        //tells players who plays first
         PlayerId currentPlayerId = gameState.currentPlayerId();
-        players.forEach((p,t) -> t.receiveInfo(pInfo.get(players.get(currentPlayerId)).willPlayFirst())); //is this the good way to do?
+        players.forEach((p, t) -> t.receiveInfo(pInfo.get(players.get(currentPlayerId)).willPlayFirst())); //is this the good way to do?
 
-        //asks players tickets to keep
-        for (PlayerId id: players.keySet()) {
-            Player p = players.get(id);
-
-            //choices
+        //tells players initial given tickets
+        for(Player p: players.values())
             p.setInitialTicketChoice(gameState.topTickets(INITIAL_TICKETS_COUNT));
-            gameState = gameState.withoutTopTickets(INITIAL_TICKETS_COUNT); //remove tickets from deck
+           gameState = gameState.withoutTopTickets(INITIAL_TICKETS_COUNT); //remove tickets from deck
 
-            pHandler.updateState(gameState);
+
+//        for (PlayerId id : players.keySet()) {
+//            Player p = players.get(id);
+//
+//            updateState(players, gameState);
+//            SortedBag<Ticket> initialTicketsChosen = p.chooseInitialTickets();
+//            gameState = gameState.withInitiallyChosenTickets(id, initialTicketsChosen);
+//            p.receiveInfo(pInfo.get(p).keptTickets(initialTicketsChosen.size()));
+//        }
+        //asks players tickets to keep
+        for (Map.Entry<PlayerId, Player> entry : players.entrySet()) {
+            Player p = entry.getValue();
+            updateState(players, gameState);
             SortedBag<Ticket> initialTicketsChosen = p.chooseInitialTickets();
-            gameState = gameState.withInitiallyChosenTickets(id, initialTicketsChosen);
+            gameState = gameState.withInitiallyChosenTickets(entry.getKey(), initialTicketsChosen);
             p.receiveInfo(pInfo.get(p).keptTickets(initialTicketsChosen.size()));
         }
 
 
         //main game loop
-        while (true){ //TODO: put ending conditions
+        while (true) { //TODO: put ending conditions
             Player currentPlayer = players.get(currentPlayerId);
-            pHandler.updateState(gameState);
+            receiveInfo(players, pInfo.get(currentPlayer).canPlay());
+            // USEFUL ?? updateState(players, gameState);
 
+            updateState(players, gameState);
+            switch (currentPlayer.nextTurn()) {
 
-            switch(currentPlayer.nextTurn()){
+                case DRAW_TICKETS:
 
-                case DRAW_CARDS:
                     SortedBag<Ticket> initialTickets = gameState.topTickets(IN_GAME_TICKETS_COUNT);
-                    pHandler.receiveInfo(pInfo.get(currentPlayer).drewTickets(IN_GAME_TICKETS_COUNT));
+                    receiveInfo(players, pInfo.get(currentPlayer).drewTickets(IN_GAME_TICKETS_COUNT));
 
                     SortedBag<Ticket> chosenTickets = currentPlayer.chooseTickets(initialTickets);
                     gameState = gameState.withChosenAdditionalTickets(initialTickets, chosenTickets);
-                    pHandler.receiveInfo(pInfo.get(currentPlayer).keptTickets(chosenTickets.size()));
+                    receiveInfo(players, pInfo.get(currentPlayer).keptTickets(chosenTickets.size()));
 
                     break;
 
+                case DRAW_CARDS: //TODO: updates and messages
+                    for (int i = 0; i < 2; i++) {
+                        //check deck not empty
+                        if (gameState.cardState().isDeckEmpty()) {
+                            gameState = gameState.withCardsDeckRecreatedIfNeeded(rng);
+                        }
+                        updateState(players, gameState);
+
+                        int slot = currentPlayer.drawSlot();
+                        if (slot == -1) {
+                            gameState.currentPlayerState().withAddedCard(gameState.topCard());
+                            gameState = gameState.withoutTopCard();
+                            receiveInfo(players, pInfo.get(currentPlayer).drewBlindCard());
+                        } else {
+                            Card faceUpCard = gameState.cardState().faceUpCard(slot);
+                            gameState.currentPlayerState().withAddedCard(faceUpCard);
+                            gameState = gameState.withDrawnFaceUpCard(slot);
+                            receiveInfo(players, pInfo.get(currentPlayer).drewVisibleCard(faceUpCard));
+                        }
+                    }
+                    break; //TODO: useless?
+
                 case CLAIM_ROUTE:
+                    Route chosenRoute = currentPlayer.claimedRoute();
+                    //TODO: do we have to check all this here??? piazza answers are unclear
+                    boolean canClaimRoute = gameState.currentPlayerState().canClaimRoute(chosenRoute);
+                    if (canClaimRoute) {
+
+                        //TODO: check if initialCards are valid to start claim???
+                        SortedBag<Card> initCards = currentPlayer.initialClaimCards();
+
+                        //by default no additional cards needed, changes if route is underground and additional cards are needed
+                        SortedBag<Card> additionalCards = SortedBag.of();
+                        int additionalCardsCount = 0;
+
+                        if (chosenRoute.level() == Route.Level.UNDERGROUND) {
+                            receiveInfo(players, pInfo.get(currentPlayer).attemptsTunnelClaim(chosenRoute, initCards));
+                            SortedBag.Builder<Card> additionalCardsTunnelBuilder = new SortedBag.Builder<>();
+                            //draw additional tunnel cards one by one...
+                            for (int i = 0; i < ADDITIONAL_TUNNEL_CARDS; i++) {
+                                //check deck not empty
+                                if (gameState.cardState().isDeckEmpty()) {
+                                    gameState = gameState.withCardsDeckRecreatedIfNeeded(rng);
+                                }
+                                additionalCardsTunnelBuilder.add(gameState.topCard());
+                                gameState = gameState.withoutTopCard();
+                            }
+                            SortedBag<Card> drawnCards = additionalCardsTunnelBuilder.build();
+                            additionalCardsCount = chosenRoute.
+                                    additionalClaimCardsCount(initCards, drawnCards);
+
+                            receiveInfo(players, pInfo.get(currentPlayer).drewAdditionalCards(drawnCards, additionalCardsCount));
+
+                            //player has to play additional cards
+                            if (additionalCardsCount >= 1) {
+                                List<SortedBag<Card>> possibleAdditionalCards = gameState.
+                                        currentPlayerState().
+                                        possibleAdditionalCards(additionalCardsCount, initCards, drawnCards);
+
+                                additionalCards = currentPlayer.chooseAdditionalCards(possibleAdditionalCards);
+                            }
+
+
+                        }
+                        if (!additionalCards.isEmpty() || additionalCardsCount == 0) {
+                            SortedBag<Card> finalClaimCards = initCards.union(additionalCards);
+                            gameState = gameState.
+                                    withClaimedRoute(chosenRoute, finalClaimCards);
+                            receiveInfo(players, pInfo.get(currentPlayer).claimedRoute(chosenRoute, finalClaimCards));
+                            updateState(players, gameState);
+                        } else
+                            receiveInfo(players, pInfo.get(currentPlayer).didNotClaimRoute(chosenRoute));
+
+                    }
+
+                    break;
             }
 
-
-
+            //change current player
+            gameState = gameState.forNextTurn();
         }
 
 
     }
-
 
     /**
-     * Handler that handles the info messages and updates the players game states
-      */
-    private static class PlayerHandler{
-        private Map<PlayerId, Player> players;
-
-        public PlayerHandler(Map<PlayerId, Player> players) {
-            this.players = players;
-        }
-
-        /**
-         * Updates players with the new game state
-         * @param newGameState
-         *          new game state
-         */
-        private void updateState(GameState newGameState){
-            players.forEach((p,t) -> t.updateState(newGameState, newGameState.playerState(p)));
-        }
-
-        /**
-         * Give info to players
-         * @param message
-         *          info to give to players
-         */
-        private void receiveInfo(String message){
-            players.values().forEach((t) -> t.receiveInfo(message));
-        }
-
+     * Updates players with the new game state
+     *
+     * @param newGameState new game state
+     */
+    private static void updateState(Map<PlayerId, Player> map, GameState newGameState) {
+        map.forEach((id, p) -> p.updateState(newGameState, newGameState.playerState(id)));
     }
 
-    // In case we dont end up using the PlayerHandler class
-//    private static void updateState(Map<PlayerId, Player> map, GameState newGameState){
-//        map.forEach((p,t) -> t.updateState(newGameState, newGameState.playerState(p)));
-//    }
-//
-//    private static void receiveInfo(Map<PlayerId, Player> map, String message){
-//        map.forEach((p,t) -> t.receiveInfo(message));
-//
-//    }
+    /**
+     * Give info to players
+     *
+     * @param message info to give to players
+     */
+    private static void receiveInfo(Map<PlayerId, Player> map, String message) {
+        map.forEach((id, p) -> p.receiveInfo(message));
+        map.values().forEach((p) -> p.receiveInfo(message));
+    }
 
 }
