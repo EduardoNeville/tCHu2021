94aaccc12df7bd7725b000fc08d6235d10897b2d
msl <martinsanchezlopez>
2021-04-08 15:01:05 +0200

    Game tests and other official tests

--- Prog2_tCHu/test/ch/epfl/tchu/game/PlayerIdTest.java
+++ Prog2_tCHu/test/ch/epfl/tchu/game/PlayerIdTest.java
@@ -1,6 +1,5 @@
 package ch.epfl.tchu.game;
 
-import ch.epfl.tchu.game.PlayerId;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,23 +7,14 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerIdTest {
-
     @Test
-    void nextWorks() {
-        PlayerId p= PlayerId.PLAYER_1;
-        assertEquals(PlayerId.PLAYER_2, p.next());
-        assertEquals(PlayerId.PLAYER_2, p.next());
-        p = p.next();
-        assertEquals(PlayerId.PLAYER_1, p.next());
-    }
-
-    @Test
-    void countCorrect() {
-        assertEquals(2, PlayerId.COUNT);
-    }
-
-    @Test
-    void allIsCorrect() {
+    void playerIdAllIsDefinedCorrectly() {
         assertEquals(List.of(PlayerId.PLAYER_1, PlayerId.PLAYER_2), PlayerId.ALL);
+    }
+
+    @Test
+    void playerIdNextWorks() {
+        assertEquals(PlayerId.PLAYER_2, PlayerId.PLAYER_1.next());
+        assertEquals(PlayerId.PLAYER_1, PlayerId.PLAYER_2.next());
     }
 }
