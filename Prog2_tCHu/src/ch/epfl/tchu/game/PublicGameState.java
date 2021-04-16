a644ba0e0e92d96f51b4b3905e1ac3b84b7851df
msl <martinsanchezlopez@users.noreply.github.com>
2021-04-16 15:43:32 +0200

    PublicGameState comments and stuff

--- Prog2_tCHu/src/ch/epfl/tchu/game/PublicGameState.java
+++ Prog2_tCHu/src/ch/epfl/tchu/game/PublicGameState.java
@@ -6,117 +6,119 @@ import java.util.*;
 
 /**
  * Class that represents the state of the game accessible to all players.
- *
+ * <p>
  * Methods: ticketCount,canDrawTickets, cardState, canDrawCards, currentPlayerId, playerState,
  * currentPlayerState,claimedRoutes,lastPlayer
  *
  * @author Eduardo Neville (314667)
  */
 public class PublicGameState {
-    private final int ticketCounT;
-    private final PublicCardState cardStatE;
+    private final int ticketCount;
+    private final PublicCardState cardState;
     private final PlayerId currentPlayerID;
-    private final  Map<PlayerId, PublicPlayerState> playerStatE;
-    private final PlayerId lastPlayeR;
+    private final Map<PlayerId, PublicPlayerState> playerState;
+    private final PlayerId lastPlayer;
 
     /**
      * Constructor for PublicGameState
-     * @param ticketsCount
-     *              # of tickets
-     * @param cardState
-     *              cards on the deck
-     * @param currentPlayerId
-     *              current player
-     * @param playerState
-     *              Map of playerId and PlayerState
-     * @param lastPlayer
-     *              last Player
-     * @throws IllegalArgumentException
-     *              thrown if ticketCount is smaller that 0
-     * @throws IllegalArgumentException
-     *              thrown if playerState size isn't the size of PlayerId.COUNT
+     *
+     * @param ticketsCount    # of tickets
+     * @param cardState       cards on the deck
+     * @param currentPlayerId current player
+     * @param playerState     Map of playerId and PlayerState
+     * @param lastPlayer      last Player
+     * @throws IllegalArgumentException thrown if ticketCount is smaller that 0
+     * @throws IllegalArgumentException thrown if playerState size isn't the size of PlayerId.COUNT
      */
     public PublicGameState(int ticketsCount, PublicCardState cardState,
                            PlayerId currentPlayerId,
-                           Map<PlayerId,PublicPlayerState> playerState,
-                           PlayerId lastPlayer){
+                           Map<PlayerId, PublicPlayerState> playerState,
+                           PlayerId lastPlayer) {
 
-        Preconditions.checkArgument(ticketsCount>=0);
-        Preconditions.checkArgument(playerState.size()== PlayerId.COUNT);
+        Preconditions.checkArgument(ticketsCount >= 0);
+        Preconditions.checkArgument(playerState.size() == PlayerId.COUNT);
 
-        this.ticketCounT = ticketsCount;
-        this.cardStatE = Objects.requireNonNull(cardState);
+        this.ticketCount = ticketsCount;
+        this.cardState = Objects.requireNonNull(cardState);
         this.currentPlayerID = Objects.requireNonNull(currentPlayerId);
-        this.playerStatE = Map.copyOf(Objects.requireNonNull(playerState));
-        this.lastPlayeR = lastPlayer;
+        this.playerState = Map.copyOf(Objects.requireNonNull(playerState));
+        this.lastPlayer = lastPlayer;
     }
 
     /**
-     * Ticket size
-     * @return ticket size
+     * Returns the amount of tickets there are.
+     *
+     * @return amount of tickets there are
      */
-    public int ticketsCount(){
-        return ticketCounT;
+    public int ticketsCount() {
+        return ticketCount;
     }
 
     /**
-     * checks if you can draw a ticket
-     * @return checks if you can draw a ticket
+     * Checks if you can draw a ticket
+     *
+     * @return true if you can draw a ticket
      */
-    public boolean canDrawTickets(){
-        return !(ticketCounT ==0);
+    public boolean canDrawTickets() {
+        return !(ticketCount == 0);
     }
 
     /**
-     * Getter for the cardState
-     * @return cardstate of the game
+     * Returns the public card state of the game.
+     *
+     * @return the public card state of the game
      */
-    public PublicCardState cardState(){
-        return cardStatE;
+    public PublicCardState cardState() {
+        return cardState;
     }
 
     /**
-     * Checks if you can draw cards
-     * @return true if you can draw a card my checking the cardState is bigger that the initial ticket count
+     * Checks if cards can be drawn by checking the cardState is bigger that the initial ticket count.
+     *
+     * @return true if you can draw a card
      */
-    public boolean canDrawCards(){
-        return (cardStatE.deckSize()+cardStatE.discardsSize()>=Constants.INITIAL_TICKETS_COUNT);
+    public boolean canDrawCards() {
+        return (cardState.deckSize() + cardState.discardsSize() >= 5);
     }
 
     /**
-     * Getter for the current PlayerId
-     * @return current PlayerId
+     * Returns the PlayerId of the current player.
+     *
+     * @return the PlayerId of the current player
      */
-    public PlayerId currentPlayerId(){
+    public PlayerId currentPlayerId() {
         return currentPlayerID;
     }
 
     /**
-     * Getter for the playerState
-     * @param playerId the playerId of the playerstate we want
-     * @return playerstate of the player we want
+     * Returns the publicPlayerState of a player.
+     *
+     * @param playerId the playerId of the player we want
+     * @return publicPlayerState of the player gave the id of
      */
     public PublicPlayerState playerState(PlayerId playerId) {
-        return playerStatE.get(playerId);
+        return playerState.get(playerId);
     }
 
     /**
-     * Getter for the currentplayerState
-     * @return the playerState of the currentplayer
+     * Returns the publicPlayerState of the current player.
+     *
+     * @return the publicPlayerState of the current player
      */
-    public PublicPlayerState currentPlayerState(){
+    public PublicPlayerState currentPlayerState() {
         return playerState(currentPlayerId());
     }
 
     /**
      * Gives the claimedRoutes of a player
-     * @return returns the routes of a player
+     *
+     * @return returns list of the routes of the players
      */
-    public List<Route> claimedRoutes(){
-        List<PublicPlayerState> allPublicPlayerState = new ArrayList<>(playerStatE.values());
+    public List<Route> claimedRoutes() {
+        List<PublicPlayerState> allPublicPlayerState = new ArrayList<>(playerState.values());
         List<Route> allRoutes = new ArrayList<>();
 
-        for (PublicPlayerState player: allPublicPlayerState) {
+        for (PublicPlayerState player : allPublicPlayerState) {
             allRoutes.addAll(player.routes());
         }
         return allRoutes;
@@ -124,9 +126,10 @@ public class PublicGameState {
 
     /**
      * Getter for the lastPlayer
-     * @return last player
+     *
+     * @return player id of the last player
      */
-    public PlayerId lastPlayer(){
-        return lastPlayeR;
+    public PlayerId lastPlayer() {
+        return lastPlayer;
     }
 }
