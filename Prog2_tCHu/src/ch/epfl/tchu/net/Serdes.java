9365c761112a05b69a34d7355ecbc91f22d3d6d7
EduardoNeville <57065449+EduardoNeville@users.noreply.github.com>
2021-04-20 18:26:32 +0200

    more series added
    We are missing: 
    PublicCardState	point-virgule (;)	 
    PublicPlayerState	point-virgule (;)	 
    PlayerState	point-virgule (;)	 
    PublicGameState	deux-points (:)

--- Prog2_tCHu/src/ch/epfl/tchu/net/Serdes.java
+++ Prog2_tCHu/src/ch/epfl/tchu/net/Serdes.java
@@ -1,7 +1,53 @@
 package ch.epfl.tchu.net;
 
+import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.game.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+/**
+ * Serdes
+ *
+ * @author Eduardo Neville (314677)
+ */
 public class Serdes {
-    public static final Serde<Integer> INTEGER = Serde.of(
+
+    //Part 1
+
+    public static final Serde<Integer> INTEGER_SERDE = Serde.of(
             i -> Integer.toString(i),
             Integer::parseInt);
+
+    public static final Serde<String> STRING_SERDE = Serde.of();
+
+    public static final Serde<PlayerId> PLAYER_ID_SERDE = Serde.oneOf(PlayerId.ALL); //TODO deserialization
+
+    public static final Serde<Player.TurnKind> TURN_KIND_SERDE = Serde.oneOf(Player.TurnKind.ALL);
+
+    public static final Serde<Card> CARD_SERDE = Serde.oneOf(Card.ALL);
+
+    public static final Serde<Route> ROUTE_SERDE = Serde.oneOf(ChMap.routes());
+
+    public static final Serde<Ticket> TICKET_SERDE = Serde.oneOf(ChMap.tickets());
+
+    //Part 2
+
+    public static final Serde<List<String>> LIST_String_SERDE = Serde.listOf(STRING_SERDE, " , ");
+
+    public static final Serde<List<Card>> LIST_CARD_SERDE = Serde.listOf(CARD_SERDE, " , ");
+
+    public static final Serde<List<Route>> LIST_ROUTE_SERDE = Serde.listOf(ROUTE_SERDE, " , ");
+
+    public static final Serde<SortedBag<Card>> SORTED_BAG_CARD_SERDE = Serde.bagOf(CARD_SERDE, " , ");
+
+    public static final Serde<SortedBag<Ticket>> SORTED_BAG_TICKET_SERDE = Serde.bagOf(TICKET_SERDE, " , ");
+
+    public static final Serde<List<SortedBag<Card>>> LIST_SORTED_BAG_CARD_SERDE = Serde.listOf(SORTED_BAG_CARD_SERDE, " ; ");
+
+    //Part 3
+    public static final Serde<PublicCardState> PUBLIC_CARD_STATE_SERDE = Serde.
+
+
+
 }
