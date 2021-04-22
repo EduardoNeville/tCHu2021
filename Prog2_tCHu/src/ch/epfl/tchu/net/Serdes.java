ae61262f827c19fe8305195a2b8cb4e2006550cd
EduardoNeville <57065449+EduardoNeville@users.noreply.github.com>
2021-04-22 10:58:32 +0200

    Added composite parts
    To be added are the deserialisation of these same parts

--- Prog2_tCHu/src/ch/epfl/tchu/net/Serdes.java
+++ Prog2_tCHu/src/ch/epfl/tchu/net/Serdes.java
@@ -4,6 +4,7 @@ import ch.epfl.tchu.SortedBag;
 import ch.epfl.tchu.game.*;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -45,9 +46,78 @@ public class Serdes {
 
     public static final Serde<List<SortedBag<Card>>> LIST_SORTED_BAG_CARD_SERDE = Serde.listOf(SORTED_BAG_CARD_SERDE, " ; ");
 
-    //Part 3
-    public static final Serde<PublicCardState> PUBLIC_CARD_STATE_SERDE = Serde.
+    // PublicCardState
+    public static final Serde<PublicCardState> PUBLIC_CARD_STATE_SERDE = new Serde<PublicCardState>() {
+        @Override
+        public String serialize(PublicCardState object) {
+            List<String> faceUpCardsStr = new ArrayList<>();
+            object.faceUpCards().forEach(o -> faceUpCardsStr.add(CARD_SERDE.serialize(o)));
 
+            return String.join(" ; ",
+                    LIST_CARD_SERDE.serialize(object.faceUpCards()) +
+                            INTEGER_SERDE.serialize(object.deckSize()) +
+                            INTEGER_SERDE.serialize(object.discardsSize()));
+            //return a string of all the different parts of the publicCardState with ; in between them
+        }
+//basically use the other serialized from serde and join them all into one string after to return
+        @Override
+        public PublicCardState deserialize(String string) {
+            LIST_CARD_SERDE.deserialize(string);
+            INTEGER_SERDE.deserialize();
+            return
+        }
+    };
+    //here the list can be any list of cards
 
+    // PublicPlayerState
+    public static final Serde<PublicPlayerState> PUBLIC_PLAYER_STATE_SERDE = new Serde<PublicPlayerState>() {
+        @Override
+        public String serialize(PublicPlayerState object) {
+            return String.join(" ; ",
+                    INTEGER_SERDE.serialize(object.ticketCount()) +
+                                INTEGER_SERDE.serialize(object.cardCount()) +
+                                LIST_ROUTE_SERDE.serialize(object.routes()));
+            //return a string of all the different parts of the publicCardState with ; in between them
+        }
+        @Override
+        public PublicPlayerState deserialize(String string) {
 
+            return string.get
+        }
+    };
+
+    public static final Serde<PlayerState> PLAYER_STATE_SERDE = new Serde<PlayerState>() {
+        @Override
+        public String serialize(PlayerState object) {
+
+            return String.join(" ; ",
+                    SORTED_BAG_TICKET_SERDE.serialize(object.tickets()) +
+                            SORTED_BAG_CARD_SERDE.serialize(object.cards()) +
+                            LIST_ROUTE_SERDE.serialize(object.routes()));
+        }
+
+        @Override
+        public PlayerState deserialize(String string) {
+
+            return string.get
+        }
+    };
+
+    public static final Serde<PublicGameState> PUBLIC_GAME_STATE_SERDE = new Serde<PublicGameState>() {
+        @Override
+        public String serialize(PublicGameState object) {
+
+            return String.join(" : ",
+                    object.ticketsCount() +
+                            PUBLIC_CARD_STATE_SERDE.serialize(object.cardState()) +
+                            PLAYER_ID_SERDE.serialize(object.currentPlayerId()) +
+                            PUBLIC_PLAYER_STATE_SERDE.serialize(object.playerState(PlayerId.PLAYER_1))+
+                            PUBLIC_PLAYER_STATE_SERDE.serialize(object.playerState(PlayerId.PLAYER_2)));
+        }
+
+        @Override
+        public PublicGameState deserialize(String string) {
+            return null;
+        }
+    };
 }
