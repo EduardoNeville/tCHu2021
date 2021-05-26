96ffccd88b5e9ad2670a8a5232a94be2bc983a64
EduardoNeville <57065449+EduardoNeville@users.noreply.github.com>
2021-05-26 19:45:21 +0200

    Skeleton code of Graph...

---
+++ Prog2_tCHu/src/ch/epfl/tchu/gui/GraphicalPlayerAdapter.java
@@ -0,0 +1,70 @@
+package ch.epfl.tchu.gui;
+
+import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.game.*;
+
+import java.util.List;
+import java.util.Map;
+
+import static javafx.application.Platform.runLater;
+
+public class GraphicalPlayerAdapter implements Player {
+    //TODO FIX this!
+    GraphicalPlayer graphicalPlayer = new GraphicalPlayer(, );
+
+    @Override
+    public void initPlayers(PlayerId ownId, Map<PlayerId, String> playerNames) {
+
+    }
+
+    @Override
+    public void setInitialTicketChoice(SortedBag<Ticket> tickets) {
+
+    }
+
+    @Override
+    public void receiveInfo(String info) {
+        runLater(() -> graphicalPlayer.receiveInfo(info));
+    }
+
+    @Override
+    public void updateState(PublicGameState newState, PlayerState ownState) {
+
+    }
+
+    @Override
+    public SortedBag<Ticket> chooseInitialTickets() {
+        return null;
+    }
+
+    @Override
+    public TurnKind nextTurn() {
+        return null;
+    }
+
+    @Override
+    public SortedBag<Ticket> chooseTickets(SortedBag<Ticket> options) {
+        runLater(() -> graphicalPlayer.chooseTickets(ts, ???));
+        return options; //TODO is this correct?
+    }
+
+    @Override
+    public int drawSlot() {
+        return 0;
+    }
+
+    @Override
+    public Route claimedRoute() {
+        return null;
+    }
+
+    @Override
+    public SortedBag<Card> initialClaimCards() {
+        return null;
+    }
+
+    @Override
+    public SortedBag<Card> chooseAdditionalCards(List<SortedBag<Card>> options) {
+        return null;
+    }
+}
