47c0b73899601115aac10f3350f038529dd00543
msl <martinsanchezlopez@users.noreply.github.com>
2021-05-28 01:03:26 +0200

    net Tests

---
+++ Prog2_tCHu/test/ch/epfl/tchu/net/ClientTest.java
@@ -0,0 +1,94 @@
+package ch.epfl.tchu.net;
+
+import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.game.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+
+public class ClientTest {
+
+    public static void main(String[] args) {
+        System.out.println("Starting client!");
+        RemotePlayerClient playerClient =
+                new RemotePlayerClient(new TestPlayer(),
+                        "localhost",
+                        5108);
+        playerClient.run();
+        System.out.println("Client done!");
+    }
+
+    private final static class TestPlayer implements Player {
+        private SortedBag<Ticket> initTickets= SortedBag.of();
+
+
+        @Override
+        public void initPlayers(PlayerId ownId,
+                                Map<PlayerId, String> names) {
+            System.out.printf("ownId: %s\n", ownId);
+            System.out.printf("playerNames: %s\n", names);
+        }
+
+        @Override
+        public void setInitialTicketChoice(SortedBag<Ticket> tickets) {
+            tickets.forEach(s -> System.out.print(s.text()));
+            System.out.println("\n");
+            initTickets = tickets;
+        }
+
+        @Override
+        public void receiveInfo(String info) {
+            System.out.printf("info string: %s\n", info);
+        }
+
+        @Override
+        public void updateState(PublicGameState newState, PlayerState ownState) {
+            System.out.printf("new PGState: %s\n", newState);
+            System.out.printf("new own PlayerState: %s\n", ownState);
+        }
+
+        @Override
+        public SortedBag<Ticket> chooseInitialTickets() {
+            System.out.print("choosing first 2 tickets of :");
+            initTickets.forEach(s -> System.out.print(s.text()));
+            System.out.println("\n");
+            return SortedBag.of(1, initTickets.get(0), 1, initTickets.get(1));
+        }
+
+        @Override
+        public TurnKind nextTurn() {
+            return TurnKind.CLAIM_ROUTE;
+        }
+
+        @Override
+        public SortedBag<Ticket> chooseTickets(SortedBag<Ticket> options) {
+            System.out.printf("choosing first too of %s\n", options);
+            return SortedBag.of(1, options.get(0),1, options.get(1));
+        }
+
+        @Override
+        public int drawSlot() {
+            System.out.println("returning slot 3");
+            return 3;
+        }
+
+        @Override
+        public Route claimedRoute() {
+            return ChMap.routes().get(1);
+        }
+
+        @Override
+        public SortedBag<Card> initialClaimCards() {
+            return SortedBag.of(1, Card.RED);
+        }
+
+        @Override
+        public SortedBag<Card> chooseAdditionalCards(List<SortedBag<Card>> options) {
+            return options.get(0);
+        }
+
+        // … autres méthodes de Player
+    }
+}
---
+++ Prog2_tCHu/test/ch/epfl/tchu/net/SerdesTest.java
@@ -0,0 +1,134 @@
+package ch.epfl.tchu.net;
+
+
+import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.game.*;
+import static ch.epfl.tchu.game.Card.*;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import static ch.epfl.tchu.game.PlayerId.PLAYER_1;
+import static ch.epfl.tchu.game.PlayerId.PLAYER_2;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SerdesTest {
+
+    @Test
+    void integerSerdeWords(){
+        int number = 135;
+        Serde<Integer> s = Serdes.INTEGER_SERDE;
+        assertEquals("135", s.serialize(number));
+        assertEquals(135, s.deserialize(s.serialize(number)));
+    }
+
+    @Test
+    void stringSerdeWorks(){
+        String string = "Test String ";
+
+        Serde<String> s = Serdes.STRING_SERDE;
+        assertEquals("Test String ", s.deserialize(s.serialize(string)));
+        assertEquals("Les méthodes getEncoder et getDecoder de la classe Base64 du paquetage",
+                s.deserialize(s.serialize("Les méthodes getEncoder et getDecoder de la classe Base64 du paquetage")));
+        assertEquals("À partir de cette étape, aucun fichier de vérification de signatures ne vous est fourni, étant donné que la signature des différentes méthodes",
+                s.deserialize(s.serialize("À partir de cette étape, aucun fichier de vérification de signatures ne vous est fourni, étant donné que la signature des différentes méthodes")));
+        assertEquals("e permet d'éviter que la chaîne de séparation ne soit interprétée comm",
+                s.deserialize(s.serialize("e permet d'éviter que la chaîne de séparation ne soit interprétée comm")));
+
+    }
+
+    @Test
+    void playerIdSerdeWorks(){
+        PlayerId p = PLAYER_1;
+        Serde<PlayerId> s = Serdes.PLAYER_ID_SERDE;
+        assertEquals("0", s.serialize(p));
+        assertEquals(p, s.deserialize(s.serialize(p)));
+        assertEquals("1", s.serialize(p.next()));
+        assertEquals(p.next(), s.deserialize(s.serialize(p.next())));
+    }
+
+    @Test
+    void turnKindSerdeWorks(){
+        Player.TurnKind k = Player.TurnKind.CLAIM_ROUTE;
+        Serde<Player.TurnKind> s = Serdes.TURN_KIND_SERDE;
+        assertEquals("2", s.serialize(k));
+        assertEquals(k, s.deserialize(s.serialize(k)));
+    }
+
+    @Test
+    void RouteSerdeWorks(){
+        Route r = ChMap.routes().get(0);
+        Serde<Route> s = Serdes.ROUTE_SERDE;
+        assertEquals("0", s.serialize(r));
+        assertEquals(r, s.deserialize(s.serialize(r)));
+    }
+
+    @Test
+    void TicketSerdeWorks(){
+        Ticket r = ChMap.tickets().get(0);
+        Serde<Ticket> s = Serdes.TICKET_SERDE;
+        assertEquals("0", s.serialize(r));
+        assertEquals(r, s.deserialize(s.serialize(r)));
+    }
+
+    @Test
+    void listString(){
+        List<String> r = List.of("dsafsad", "test2");
+        Serde<List<String>> s = Serdes.LIST_String_SERDE;
+        System.out.println(s.serialize(r));
+//        assertEquals("0", s.serialize(r));
+        assertEquals(r, s.deserialize(s.serialize(r)));
+    }
+
+    @Test
+    void cardString(){
+        List<Card> r = List.of(Card.BLUE, Card.LOCOMOTIVE);
+        Serde<List<Card>> s = Serdes.LIST_CARD_SERDE;
+        System.out.println(s.serialize(r));
+//        assertEquals("0", s.serialize(r));
+        assertEquals(r, s.deserialize(s.serialize(r)));
+    }
+
+    @Test
+    void sortedBagList(){
+        List<SortedBag<Card>> r = List.of(SortedBag.of(Card.BLUE), SortedBag.of(2, Card.LOCOMOTIVE,3,  Card.BLACK));
+        Serde<List<SortedBag<Card>>> s = Serdes.LIST_SORTED_BAG_CARD_SERDE;
+        System.out.println(s.serialize(r));
+//        assertEquals("0", s.serialize(r));
+        assertEquals(r, s.deserialize(s.serialize(r)));
+    }
+
+    @Test
+    void worksOnPlayerState(){
+        PlayerState r = PlayerState.initial(SortedBag.of(2, Card.BLUE, 2, LOCOMOTIVE));
+        Serde<PlayerState> s= Serdes.PLAYER_STATE_SERDE;
+//        assertEquals(r, s.deserialize(s.serialize(r)));
+        System.out.println(s.serialize(r));
+        assert(r.cards().equals(s.deserialize(s.serialize(r)).cards()));
+
+    }
+
+    @Test
+    void knownExampleWorks(){
+        List<Card> fu = List.of(RED, WHITE, BLUE, BLACK, RED);
+        PublicCardState cs = new PublicCardState(fu, 30, 31);
+        List<Route> rs1 = ChMap.routes().subList(0, 2);
+        Map<PlayerId, PublicPlayerState> ps = Map.of(
+                PLAYER_1, new PublicPlayerState(10, 11, rs1),
+                PLAYER_2, new PublicPlayerState(20, 21, List.of()));
+        PublicGameState gs =
+                new PublicGameState(40, cs, PLAYER_2, ps, null);
+        Serde<PublicGameState> s = Serdes.PUBLIC_GAME_STATE_SERDE;
+        assertEquals("40:6,7,2,0,6;30;31:1:10;11;0,1:20;21;:", s.serialize(gs));
+//        assertEquals(gs, s.deserialize(s.serialize(gs)));
+        assertEquals(gs.currentPlayerState().carCount(),
+                s.deserialize(s.serialize(gs)).currentPlayerState().carCount());
+        assert(gs.claimedRoutes().equals(
+                s.deserialize(s.serialize(gs)).claimedRoutes()));
+        assert s.deserialize(s.serialize(gs)).lastPlayer() == null;
+    }
+}
