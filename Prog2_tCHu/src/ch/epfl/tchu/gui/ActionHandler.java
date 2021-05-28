b33f9e37e38ea8e8a10bd26b2d2584bea2467e92
msl <martinsanchezlopez@users.noreply.github.com>
2021-05-28 16:11:58 +0200

    ActionHandler comments

--- Prog2_tCHu/src/ch/epfl/tchu/gui/ActionHandler.java
+++ Prog2_tCHu/src/ch/epfl/tchu/gui/ActionHandler.java
@@ -3,27 +3,44 @@ package ch.epfl.tchu.gui;
 import ch.epfl.tchu.SortedBag;
 import ch.epfl.tchu.game.*;
 
-public interface ActionHandler { //TODO does it have a <T extends Events> ?
+/**
+ * Interface that has different functional interfaces intended as handlers for various game actions.
+ *
+ * @author Eduardo Neville (314677)
+ */
+public interface ActionHandler {
 
-    //TODO do these interfaces need to be abstract, public or both?
-    // -> They are both by default
-
+    /**
+     * Action handler for drawing tickets
+     */
     interface DrawTicketsHandler{
         void onDrawTickets();
     }
 
+    /**
+     * Action handler for drawing cards
+     */
     interface DrawCardHandler{
         void onDrawCard(int slot); //0-4 or -1 for deck
     }
 
+    /**
+     * Action handler for claiming routes
+     */
     interface ClaimRouteHandler{
         void onClaimRoute(Route route, SortedBag<Card> cardDeck);
     }
 
+    /**
+     * Action handler for choosing tickets
+     */
     interface ChooseTicketsHandler{
         void onChooseTickets(SortedBag<Ticket> tickets);
     }
 
+    /**
+     * Action handler for choosing cards
+     */
     interface ChooseCardsHandler{
         void onChooseCards(SortedBag<Card> cards);
     }
